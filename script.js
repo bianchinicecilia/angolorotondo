03af2433763539530548ed59e18c3daf06e6d7bb
biachinicecilia <bianchini.cecilia@gmail.com>
2020-04-08 16:59:57 +0100

    updates

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const innerCircleRad = 50;
-const outerCircleRad = 272 - innerCircleRad;
-let ww =  window.innerWidth / 2,
-    wh = window.innerHeight / 2;
-window.addEventListener('resize', function(event){
-  ww =  window.innerWidth / 2,
-  wh = window.innerHeight / 2;
-});
+let innerCircleRad, outerCircleRad, ww, wh;
+
+window.addEventListener('resize', getElementSizes);
+function getElementSizes() {
+  let innerCircle = document.querySelector('.circle');
+  let outerCircle = document.querySelector('.uterus');
+
+  innerCircleRad = innerCircle.clientHeight / 2;
+  outerCircleRad = outerCircle.clientHeight / 2;
+  ww = outerCircle.offsetLeft + outerCircleRad, // center of outer circle
+  wh = outerCircle.offsetTop + outerCircleRad;
+}
+getElementSizes();
 
 document.addEventListener('mousemove', handler);
 
@@ -27,4 +32,32 @@ function handler(e) {
   }
   //document.querySelector('#inner-circle').style.transform = 'translate(' + (xoffset - innerCircleRad) + 'px, ' + (yoffset - innerCircleRad) + 'px)';
   gsap.to(".circle", {duration: 0.25, x: (xoffset - innerCircleRad), y: (yoffset - innerCircleRad)});
+}
+
+/* MODAL */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
