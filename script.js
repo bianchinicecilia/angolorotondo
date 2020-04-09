b375fe30574903e36132a16de3388239ba612f37
biachinicecilia <bianchini.cecilia@gmail.com>
2020-04-09 19:39:44 +0100

    goBack

--- script.js
+++ script.js
@@ -13,19 +13,17 @@ function getElementSizes() {
 getElementSizes();
 
 document.addEventListener('mousemove', handler);
-/*document.addEventListener('touchmove', handler);*/
+
 function handler(e) {
   e = e || window.event;
-  /*if('touches' in e) {
-    e = e.touches[0];
-  }*/
-  let mousex = e.pageX;
-  let mousey = e.pageY;
 
-  let distance = Math.hypot(mousex - ww, mousey - wh);
+  let mx = e.pageX;
+  let my = e.pageY;
 
-  let xoffset = mousex - ww;
-  let yoffset = mousey - wh;
+  let distance = Math.hypot(mx - ww, my - wh);
+
+  let xoffset = mx - ww;
+  let yoffset = my - wh;
 
   if (distance > outerCircleRad - innerCircleRad) {
     let scaleDown = (outerCircleRad - innerCircleRad) / distance;
@@ -33,5 +31,33 @@ function handler(e) {
     yoffset *= scaleDown;
   }
   //document.querySelector('#inner-circle').style.transform = 'translate(' + (xoffset - innerCircleRad) + 'px, ' + (yoffset - innerCircleRad) + 'px)';
-  gsap.to('.circle', {duration: 0.25, x: (xoffset), y: (yoffset)});
+  gsap.to(".circle", {duration: 0.25, x: xoffset, y: yoffset});
+}
+
+/* MODAL */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
